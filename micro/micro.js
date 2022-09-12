06f4b46856388112d2dced80c8b786c9f79101c7
sergromm <sergichev@gmail.com>
2022-09-12 19:43:48 +0300

    interpert and evaluate operations

--- micro/micro.js
+++ micro/micro.js
@@ -1,17 +1,10 @@
-const {
-  str,
-  digits,
-  choice,
-  between,
-  sequenceOf,
-  lazy,
-} = require('../index.js');
+const { str, digits, choice, between, sequenceOf, lazy } = require('../index.js');
 
 const betweenBrackets = between(str('('), str(')'));
 
 const numberParser = digits.map((value) => ({
-  type: 'number',
-  value: Number(value),
+	type: 'number',
+	value: Number(value),
 }));
 
 const operatorParser = choice([str('+'), str('-'), str('*'), str('/')]);
@@ -19,18 +12,46 @@ const operatorParser = choice([str('+'), str('-'), str('*'), str('/')]);
 const expr = lazy(() => choice([numberParser, operationParser]));
 
 const operationParser = betweenBrackets(
-  sequenceOf([operatorParser, str(' '), expr, str(' '), expr])
+	sequenceOf([operatorParser, str(' '), expr, str(' '), expr]),
 ).map(([operator, _, left, __, right]) => ({
-  type: 'operation',
-  value: {
-    operator,
-    left,
-    right,
-  },
+	type: 'operation',
+	value: {
+		operator,
+		left,
+		right,
+	},
 }));
 
-const example = '(+ (* 10 2) (- (/ 50 3) 2))';
+const evaluate = (node) => {
+	if (node.type === 'number') {
+		return node.value;
+	}
 
-const result = expr.run(example);
+	if (node.type === 'operation') {
+		if (node.value.operator === '+') {
+			return evaluate(node.value.left) + evaluate(node.value.right);
+		}
+		if (node.value.operator === '-') {
+			return evaluate(node.value.left) - evaluate(node.value.right);
+		}
+		if (node.value.operator === '*') {
+			return evaluate(node.value.left) * evaluate(node.value.right);
+		}
+		if (node.value.operator === '/') {
+			return evaluate(node.value.left) / evaluate(node.value.right);
+		}
+	}
+};
 
-console.log(result);
+const program = '(+ (* 10 2) (- (/ 50 3) 2))';
+
+const inerpreter = (program) => {
+	const parseResult = expr.run(program);
+	if (parseResult.isError) {
+		throw new Error("Couldn't interpret program");
+	}
+
+	return evaluate(parseResult.result);
+};
+
+console.log(inerpreter(program));
